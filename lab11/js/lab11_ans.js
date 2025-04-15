443d31fcb96291975479a4d5bb5f50e892877318
hllau <lauholam@gmail.com>
2025-04-15 11:44:25 +0800

    lab11

--- lab11/js/lab11_ans.js
+++ lab11/js/lab11_ans.js
@@ -1,5 +1,24 @@
+// step 1. find the actors
+var myH1 = document.getElementById('coursecode'); // Get the h1 element
+var myH2 = document.getElementById('newacc'); // Get the new account button
+// step 2. translate the action in terms on HTML/CSS
+function changeColor() { // Declare function
+  myH1.style.color = 'yellow'; 
+  myH1.textContent = 'Hahahaha!'; 
+  myH2.textContent = ''; 
+} // End of function
+// step 3. bind the event listener
+myH1.addEventListener('click', changeColor, false); // Add event listener
+// When 'myH1' is being 'clicked', execute 'changeColor'
+
+
+
+
+
+
 function checkUsername() {                        // Declare function
-  var username = el.value;                        // Store username in variable
+  var username = el.value;  
+  var a=0;                      // Store username in variable
   if (username.length < 5) {                      // If username < 5 characters
     elMsg.className = 'warning';                  // Change class on message
     elMsg.textContent = 'too short...';				// Update message
@@ -13,8 +32,11 @@ function tipUsername() {                          // Declare function
   elMsg.textContent = 'Username must be at least 5 characters'; // Add message
 }
 
+// In the document, get me the element by recongizing the id: username
 var el = document.getElementById('username');     // Username input
 var elMsg = document.getElementById('feedback');  // Element to hold message
+
+
 
 // When the username input gains / loses focus call functions above:
 el.addEventListener('focus', tipUsername, false); // focus call tipUsername()
